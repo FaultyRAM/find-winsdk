9e3edc93d4db4ccddd20efb6e5fab40ac404e243
FaultyRAM <explodingpsu@gmail.com>
2018-07-01 11:51:48 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# find-winsdk
+
+[![Travis CI](https://travis-ci.com/FaultyRAM/find-winsdk.svg)][1]
+[![AppVeyor](https://ci.appveyor.com/api/projects/status/t9b8mlp7y1ro06q4?retina=true&svg=true)][2]
+[![Crates.io](https://img.shields.io/crates/v/find-winsdk.svg)][3]
+[![Docs.rs](https://docs.rs/find-winsdk/badge.svg)][4]
+
+Provides support for detecting Windows SDK installations.
+
+## License
+
+Licensed under either of
+
+* Apache License, Version 2.0,
+  ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+* MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally
+submitted for inclusion in the work by you, as defined in the Apache-2.0
+license, shall be dual licensed as above, without any additional terms or
+conditions.
+
+[1]: https://travis-ci.com/FaultyRAM/find-winsdk
+[2]: https://ci.appveyor.com/project/FaultyRAM/find-winsdk
+[3]: https://crates.io/crates/find-winsdk
+[4]: https://docs.rs/find-winsdk
---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+// Copyright (c) 2018 FaultyRAM
+//
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the
+// MIT license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at
+// your option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! Provides support for detecting Windows SDK installations.
+
+#![cfg(target_os = "windows")]
+#![forbid(warnings)]
+#![forbid(future_incompatible)]
+#![forbid(unused)]
+#![forbid(box_pointers)]
+#![forbid(missing_copy_implementations)]
+#![forbid(missing_debug_implementations)]
+#![forbid(missing_docs)]
+#![forbid(trivial_casts)]
+#![forbid(trivial_numeric_casts)]
+#![forbid(unsafe_code)]
+#![forbid(unused_import_braces)]
+#![deny(unused_qualifications)]
+#![forbid(unused_results)]
+#![forbid(variant_size_differences)]
+#![cfg_attr(feature = "cargo-clippy", forbid(clippy))]
+#![cfg_attr(feature = "cargo-clippy", forbid(clippy_pedantic))]
+#![cfg_attr(feature = "cargo-clippy", forbid(clippy_cargo))]
+#![cfg_attr(feature = "cargo-clippy", forbid(clippy_complexity))]
+#![cfg_attr(feature = "cargo-clippy", forbid(clippy_correctness))]
+#![cfg_attr(feature = "cargo-clippy", forbid(clippy_perf))]
+#![cfg_attr(feature = "cargo-clippy", forbid(clippy_style))]
